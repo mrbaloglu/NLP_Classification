c065870ed34d761dd130b8fea497284ae74da047
emrebaloglu <emre.baloglu@soostone.com>
2022-07-02 16:46:19 +0300

    added preprocessing module for cleaning, tokenizing and padding textual input on csv files.

---
+++ main.py
@@ -0,0 +1,15 @@
+from preprocessing import *
+import pandas as pd
+
+if __name__ == '__main__':
+
+    # preprocessing the data
+    data = pd.read_csv("rt-polarity-full.csv")
+    data.columns = ['label', 'review']
+    data_prc = process_df_texts(data, ["review"])
+    data_tkn = tokenize_data(data, ["review"], preprocess=True)
+    data_sqn = pad_tokenized_data(data_tkn, ["review_tokenized"])
+
+    # save the processed data in a csv file
+    data_sqn.to_csv("rt-polarity-processed.csv")
+
