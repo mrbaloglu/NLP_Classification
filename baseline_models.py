dc3e02ec43978dc0d59acd91f2d1b691c40e657a
emrebaloglu <emre.baloglu@soostone.com>
2022-07-08 19:09:28 +0300

    added transformer

--- baseline_models.py
+++ baseline_models.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from typing import Union
 
 class RNN_Baseline_Model(nn.Module):
     def __init__(self,
@@ -15,15 +16,15 @@ class RNN_Baseline_Model(nn.Module):
                  units: int = 50):
         """
         RNN Class for text classification.
-        The argument "vocab_size" specifies the size of the vocabulary to be used in word embeddings.
-        The argument "input_shape" specifies the shape of the features (n_samples, max_sentence_length?). TODO check
-        The argument "embed_dim" specifies the embedding dimension for the categorical part of the input.
-        The argument "rnn_type" specifies the type of the recurrent layer for word embeddings.
+        :param vocab_size specifies the size of the vocabulary to be used in word embeddings.
+        :param input_shape specifies the shape of the features (n_samples, max_sentence_length?). TODO check
+        :param embed_dim specifies the embedding dimension for the categorical part of the input.
+        :param rnn_type specifies the type of the recurrent layer for word embeddings.
         MUST be "gru" or "lstm".
-        The argument "rnn_hidden_size" specifies the number of stacked recurrent layers.
-        The argument "rnn_hidden_out" specifies number of features in the hidden state h of recurrent layer.
-        The argument "rnn_bidirectional" specifies whether the recurrent layers be bidirectional.
-        The argument "units" specifies the number of neurons in the hidden layers.
+        :param rnn_hidden_size specifies the number of stacked recurrent layers.
+        :param rnn_hidden_out specifies number of features in the hidden state h of recurrent layer.
+        :param rnn_bidirectional specifies whether the recurrent layers be bidirectional.
+        :param units specifies the number of neurons in the hidden layers.
         """
         super(RNN_Baseline_Model, self).__init__()
         self.embed_dim = embed_dim
@@ -61,14 +62,74 @@ class RNN_Baseline_Model(nn.Module):
             x = torch.unsqueeze(x, 0)
         
         x_c = self.embed_enc(x)
-        # print(f"After embedding x_cat shape: {x_c.shape}")
         x_c, _ = self.rnn(x_c)
         x_c = self.flat(x_c)
 
         x = torch.cat((x_c, x_num), 1)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
-        x = self.fc3(x)
+        x = F.softmax(self.fc3(x))
 
         # TODO figure out the embedding transformations
         return x
+
+
+ 
+class Transformer_Baseline_Model(nn.Module):
+    def __init__(self,
+                 vocab_size: int,
+                 input_shape: tuple,
+                 out_num_classes: int,
+                 embed_dim: int = 5):
+        """
+        Transformer Encoder Class for text classification.
+        :param vocab_size specifies the size of the vocabulary to be used in word embeddings.
+        :param input_shape specifies the shape of the features (n_samples, max_sentence_length?). TODO check
+        :param embed_dim specifies the embedding dimension for the categorical part of the input.
+        """
+        super(Transformer_Baseline_Model, self).__init__()
+
+        self.embed_dim = embed_dim
+        self.embed_enc = nn.Embedding(vocab_size, embed_dim, max_norm=True)
+        self.embedding, num_embeddings, embedding_dim = create_emb_layer(weights_matrix, freeze_emb)
+        print(num_embeddings, embedding_dim)
+        trans_enc_layer = nn.TransformerEncoderLayer(d_model = embedding_dim, nhead = 5 ,batch_first=True)
+        self.transformer = nn.TransformerEncoder(trans_enc_layer, num_layers=16)
+        self.fc1 = nn.Linear(18600, out_num_classes) # TODO find out dimensions
+        self.flatten = nn.Flatten()
+
+    def forward(self, x: Union[np.ndarray, torch.Tensor]) -> torch.Tensor:
+        """
+        Function for model architecture.
+        :param x: np.ndarray or torch.Tensor (n_samples, max_sentence_length)
+        :return prediction for class (n_samples, out_num_classes)
+        """
+
+        # type check
+        if type(x) != torch.Tensor:
+            x = torch.Tensor(x).to(device).int()
+        # reshape when there is only one sample
+        if len(x.shape) == 1:
+            x = torch.unsqueeze(x, 0)
+        
+        x = self.embedding(x)
+        #print(x.shape)
+        x = self.transformer(x)
+        #print(x.shape)
+        #print(cn, hn.shape, 2)
+        x = self.flatten(x)
+        #print(x.shape)
+        x = F.softmax(self.fc1(x))
+        return x
+
+    def embout(self, x):
+        return self.embedding(x)
+
+    # freeze the layers until given index
+    def freeze_layers(self, index):
+        cnt = 0
+        for p in self.parameters():
+            p.requires_grad = False
+            cnt += 1
+            if(cnt == index):
+              break
